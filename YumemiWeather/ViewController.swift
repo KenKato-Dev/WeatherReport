b7803511d6dedbf0fe93d9bd20eaf0dba548d450
[katoken] <[ken0113_dev@icloud.com]>
2022-03-05 20:15:24 +0900

    Initial Commit

---
+++ YumemiWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YumemiWeather
+//
+//  Created by 加藤研太郎 on 2022/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
